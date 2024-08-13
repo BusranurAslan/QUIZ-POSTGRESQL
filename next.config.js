52381584f1ee0876551b820cb2a5b4bc8ba5ba95
BusranurAslan <97465445+BusranurAslan@users.noreply.github.com>
2024-08-13 23:14:44 +0300

    next.config.js edit

--- next.config.js
+++ next.config.js
@@ -1,38 +1,14 @@
-const nextConfig = {
-    i18n: {
-      locales: ['en', 'fr'], // i18n (internationalization) ayarlarınız
-      defaultLocale: 'en',
-    },
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve = {
-          ...config.resolve,
-          fallback: {
-            ...config.resolve.fallback,
-            fs: false,
-          },
-        };
-      }
-  
-      config.module = {
-        ...config.module,
-        exprContextCritical: false,
-      };
-  
-   
-      config.module.rules.push({
-        test: /\.md$/,
-        use: 'raw-loader',
-      });
-  
-      return config;
-    },
+require('dotenv').config(); // .env dosyasını yükle
 
-    reactStrictMode: true,
-    images: {
-      domains: ['example.com'],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+};
+
+module.exports = nextConfig;
